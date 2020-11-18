72b51cae56a0410b55260663446c8b4002f1b415
Joao <joao.g.leal@gmail.com>
2020-11-18 16:12:59 -0300

    added support for multiple source directories

--- envs_backup.py
+++ envs_backup.py
@@ -2,16 +2,24 @@ import os
 from datetime import datetime
 import re
 
-# envs_dir = r"dir containing conda envs folders"
+# envs_dir = [r"list of dirs containing conda envs folders"]
 # backups_dir = r"directory to store the backups"
 
-envs_dir = r"/home/joao/anaconda3/envs"
+envs_dirs = [r"/home/joao/anaconda3/envs"]
 backups_dir = r"/home/joao/Documents/python_envs_backups"
+dir_age = 365
 
-envs_names = [name for name in os.listdir(envs_dir) if os.path.isdir(os.path.join(envs_dir, name))]
+envs_names = []
+for _dir in envs_dirs:
+	envs = os.listdir(_dir)
+
+	for env in envs:
+		if os.path.isdir(os.path.join(_dir, env)):
+			envs_names.append(env)
+
 month_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-# Cria diretório para backup do dia
+# Create directory for backup of the day
 current_date = datetime.now()
 year_dir = os.path.join(backups_dir, str(current_date.year))
 month_dir = os.path.join(year_dir, current_date.strftime('%B'))
@@ -24,7 +32,7 @@ if not os.path.isdir(month_dir):
 if not os.path.isdir(day_dir):
 	os.mkdir(day_dir)
 
-# Verifica quantos diretórios já tem mais de 1 ano
+# Checks which directories are older than variable dir_age
 last_day = False
 last_month = False
 for root, dirs, files in os.walk(backups_dir):
@@ -50,12 +58,12 @@ for root, dirs, files in os.walk(backups_dir):
 	elif re.match('\d{1,2}', current_dir):
 		day = int(current_dir)
 
-	# Checa a data do diretório do dia
+	# Checks date of current directory
 	if dirs == []:
 		dir_date = datetime(year, month_number, day)
 		date_diff = current_date - dir_date
 
-		if date_diff.days > 365:
+		if date_diff.days > dir_age:
 			for file in files:
 				os.remove(os.path.join(root, file))
 			os.rmdir(root)
@@ -66,11 +74,11 @@ for root, dirs, files in os.walk(backups_dir):
 				if last_month:
 					os.rmdir(os.path.dirname(os.path.dirname(root)))
 
-# Faz o backup das envs na pasta do dia
+# Creates backups for each env on the current date folder
 for env in envs_names:
 	env_file = os.path.join(day_dir, f'{env}.yml')
 	os.system(f'conda activate {env} && conda env export > "{env_file}" && conda deactivate')
 
-# Faz backup do ambiente base
+# Creates backup of base environment
 base_env_file = os.path.join(day_dir, 'base.yml')
 os.system(f'conda activate base && conda env export > "{base_env_file}" && conda deactivate')
